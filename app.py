fd7e3052a7d19c30d522a5ea7eedb08069058dcd
samschicklucas-beep <samschicklucas@icloud.com>
2026-06-07 22:33:03 -0400

    app.py

--- app.py
+++ app.py
@@ -390,6 +390,7 @@ def debug():
     except Exception as e:
         return jsonify({"error": str(e)})
 
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
